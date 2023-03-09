0f14151746ab76d59bb7f8fb494618a274f7bbe5
Etec Profº Horácio Augusto da Silveira <erickpereiramassuco@gmail.com>
2023-03-08 22:32:31 -0300

    Teste de projeto para mostrar mensagem na tela

---
+++ app/src/main/java/com/ds2/sendmessages/DisplayMessageActivity.kt
@@ -0,0 +1,16 @@
+package com.ds2.sendmessages
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.AlarmClock.EXTRA_MESSAGE
+import android.widget.TextView
+
+class DisplayMessageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_display_message)
+        val displayMessageText = findViewById<TextView>(R.id.displayMessageText)
+        val message = intent.getStringExtra(EXTRA_MESSAGE)
+        displayMessageText.apply { text = message }
+    }
+}
